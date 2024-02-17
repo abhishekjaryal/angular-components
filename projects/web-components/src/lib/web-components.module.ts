59fb03e64f2977ed2fc281a9ce2826ae55856d4d
abjaryal <jaryal.jerry@gmail.com>
2024-02-17 22:29:28 +0530

    Initial commit

---
+++ projects/web-components/src/lib/web-components.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { WebComponentsComponent } from './web-components.component';
+
+
+
+@NgModule({
+  declarations: [
+    WebComponentsComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    WebComponentsComponent
+  ]
+})
+export class WebComponentsModule { }
